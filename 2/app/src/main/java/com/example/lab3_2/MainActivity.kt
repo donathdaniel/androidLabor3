888e8eed52e4ef1db45d30a78d3fd17fff1b8a70
donathdaniel <57301890+donathdaniel@users.noreply.github.com>
2021-10-12 19:08:50 +0300

    Finished

--- 2/app/src/main/java/com/example/lab3_2/MainActivity.kt
+++ 2/app/src/main/java/com/example/lab3_2/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.lab3_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+
+        button.setOnClickListener {
+            if(editText.text.isEmpty()){
+                Toast.makeText(applicationContext, "You did not give a name!", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(applicationContext, "Your name: ${editText.text}", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
